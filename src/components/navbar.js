e36c7f50ba1541a600d52304a214eec156ad7f46
Jhonatam Nogueira dos Santos <jhonatamsty.096@gmail.com>
2022-04-13 21:45:05 -0300

    delete post frame

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -73,9 +73,6 @@ const Navbar = props => {
           <LinkItem href='/works' path={path}>
             works
           </LinkItem>
-          <LinkItem href='/posts' path={path}>
-            posts
-          </LinkItem>
         </Stack>
 
         <Box flex={1} align='right'>
